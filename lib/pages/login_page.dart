2b1be96e5bfdd4617c1420ca6d97876081139580
div1681 <g.divyansh@iitg.ac.in>
2025-06-15 11:52:43 +0530

    updated UI

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -14,120 +14,124 @@ class LoginPage extends StatelessWidget {
       body: //
       SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 40),
-              Icon(Icons.lock, size: 75),
-              SizedBox(height: 40),
-              Text('Welcome back!'),
-              SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 0),
+                Icon(Icons.lock, size: 75),
+                SizedBox(height: 40),
+                Text('Welcome back!'),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[400]!),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Username',
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey[400]!),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Username',
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[400]!),
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Password',
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey[400]!),
-                    ),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Password',
                   ),
                 ),
-              ),
 
-              SizedBox(height: 50),
+                SizedBox(height: 50),
 
-              GestureDetector(
-                onTap: () => signinuser(),
-                child: Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.symmetric(horizontal: 25),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "SIGN IN",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                GestureDetector(
+                  onTap: () => signinuser(),
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "SIGN IN",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
 
-              SizedBox(height: 50),
+                SizedBox(height: 50),
 
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: Row(
-                  children: [
-                    Expanded(child: Divider(thickness: 0.5)),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                      child: Text('Or continue with'),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    children: [
+                      Expanded(child: Divider(thickness: 0.5)),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Text('Or continue with'),
+                      ),
+                      Expanded(child: Divider(thickness: 0.5)),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 40),
+                GestureDetector(
+                  onTap: () => googlesignin(),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(color: Colors.white),
                     ),
-                    Expanded(child: Divider(thickness: 0.5)),
+                    child: Image.asset(
+                      'assets/images/google_logo.png',
+                      height: 36,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Not a member? '),
+                    const SizedBox(width: 4),
+                    Text(
+                      'Registed now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
-              ),
-              SizedBox(height: 40),
-              GestureDetector(
-                onTap: () => googlesignin(),
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(color: Colors.white),
-                  ),
-                  child: Image.asset(
-                    'assets/images/google_logo.png',
-                    height: 36,
-                  ),
-                ),
-              ),
-              SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Not a member? '),
-                  const SizedBox(width: 4),
-                  Text(
-                    'Registed now',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+                SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
